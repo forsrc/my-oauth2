aa549576a89faa094ccf8d3c148e3c84c662fd5d
forsrc <forsrc@gmail.com>
2020-12-02 12:12:54 +0900

    eureka-server

--- oauth2-eureka-server/src/main/java/com/forsrc/oauth2/eureka/config/WebSecurityConfig.java
+++ oauth2-eureka-server/src/main/java/com/forsrc/oauth2/eureka/config/WebSecurityConfig.java
@@ -9,7 +9,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-		.authorizeRequests().antMatchers("/actuator/**")
+		.authorizeRequests()
+		.antMatchers("/actuator/**")
 		.permitAll()
 		.and()
 		.csrf()
---
+++ oauth2-gateway/src/main/java/com/forsrc/oauth2/gateway/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.forsrc.oauth2.gateway.controller;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+ 
+
+@Controller
+public class HomeController {
+
+
+	@RequestMapping("/")
+    public String test(Principal principal) {
+        return "/index";
+                
+    }
+}
