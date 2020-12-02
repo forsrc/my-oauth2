a88958b2af78da5b576aac4178a4facf68dbc592
forsrc <forsrc@gmail.com>
2020-12-02 17:17:18 +0900

    Hystrix Dashboard

--- oauth2-eureka-server/src/main/java/com/forsrc/oauth2/eureka/Oauth2EurekaServer.java
+++ oauth2-eureka-server/src/main/java/com/forsrc/oauth2/eureka/Oauth2EurekaServer.java
@@ -5,10 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableEurekaServer
 @EnableDiscoveryClient
 @EnableEurekaClient
+@EnableHystrixDashboard
+@EnableHystrix
 @SpringBootApplication
 public class Oauth2EurekaServer {
 
