45a2594c43ee6bf27a17bc7314168061d181e3d9
forsrc <forsrc@gmail.com>
2020-12-12 00:07:20 +0900

    yml

--- oauth2-server/src/main/java/com/forsrc/oauth2/server/controller/UserInfoController.java
+++ oauth2-server/src/main/java/com/forsrc/oauth2/server/controller/UserInfoController.java
@@ -1,28 +1,21 @@
 package com.forsrc.oauth2.server.controller;
 
-import java.security.Principal;
-
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
-@RequestMapping("/user")
-public class UserController {
+public class UserInfoController {
 
 
-    @GetMapping(path = "/me/", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/oauth/user_info", produces = MediaType.APPLICATION_JSON_VALUE)
     //@PreAuthorize("isAuthenticated()")
     public ResponseEntity<Principal> user(Principal principal) {
         System.out.println("-> Principal: " + principal);
         return ResponseEntity.ok(principal);
-    }
-
-    @GetMapping("/test")
-    public String test() {
-        System.out.println("-> test");
-        return "test";
     }
 }
