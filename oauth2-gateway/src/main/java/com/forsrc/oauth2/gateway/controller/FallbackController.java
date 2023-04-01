1c46619e262d908f35434ccc51cae4da8038cd41
forsrc <forsrc@gmail.com>
2023-04-01 22:54:23 +0900

    springboot 2.7.0

--- oauth2-gateway/src/main/java/com/forsrc/oauth2/gateway/controller/FallbackController.java
+++ oauth2-gateway/src/main/java/com/forsrc/oauth2/gateway/controller/FallbackController.java
@@ -1,17 +1,18 @@
 package com.forsrc.oauth2.gateway.controller;
 
-import com.netflix.hystrix.exception.HystrixTimeoutException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.ServerWebExchangeDecorator;
-import reactor.core.publisher.Mono;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.netflix.hystrix.exception.HystrixTimeoutException;
+
+import reactor.core.publisher.Mono;
 
 @RestController
 public class FallbackController {
@@ -20,14 +21,14 @@ public class FallbackController {
     @ResponseStatus
     public Mono<Map<String, Object>> fallback(ServerWebExchange exchange, Throwable throwable) {
         Map<String, Object> result = new HashMap<>(4);
-        Exception exception = exchange.getAttribute(ServerWebExchangeUtils.HYSTRIX_EXECUTION_EXCEPTION_ATTR);
+        Exception exception = exchange.getAttribute(ServerWebExchangeUtils.CIRCUITBREAKER_EXECUTION_EXCEPTION_ATTR);
         ServerWebExchange delegate = ((ServerWebExchangeDecorator) exchange).getDelegate();
 
         result.put("path", delegate.getRequest().getURI());
         result.put("method", delegate.getRequest().getMethodValue());
         if (exception instanceof HystrixTimeoutException) {
             result.put("message", exception.getMessage());
-            result.put("exception", "HystrixTimeoutException");
+            result.put("exception", "circuitBreakerExecutionException");
         } else if (exception != null) {
             result.put("message", exception.getMessage());
             result.put("exception", exception.getClass());
